240acf617a7666eda55fa76a4c816cc439cd314d
Andres Lopez <166462520+AndresLv04@users.noreply.github.com>
2024-10-28 14:02:42 +0000

    Modificación

--- JuegoLucha/Ejecutar.java
+++ JuegoLucha/Ejecutar.java
@@ -1,11 +1,20 @@
 package JuegoLucha;
 
+import java.util.Scanner;
+
 public class Ejecutar {
     public static void main(String[] args) {
-    
+        
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.println("Nombre para el jugador 1");
+        String jNombre1 = sc.nextLine();
+        System.out.println("Nombre para el jugador 2");
+        String jNombre2 = sc.nextLine();
 
-        Personaje objGuerrero = new Guerrero("Andres");
-        Personaje objMago = new Mago("Cano");
+
+        Personaje objGuerrero = new Guerrero(jNombre1);
+        Personaje objMago = new Mago(jNombre2);
 
         JuegoLucha objJuego = new JuegoLucha(objGuerrero, objMago);
         objJuego.iniciarPelea();
