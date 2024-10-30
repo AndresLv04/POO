6e9dc96190e6ee53fef58593fc72c89ef76e8c34
Andres Lopez <166462520+AndresLv04@users.noreply.github.com>
2024-10-30 07:22:22 +0000

    Agregando método de iluminación, sonido y mejorando el de presionar

--- Momento3/Boton.java
+++ Momento3/Boton.java
@@ -4,10 +4,12 @@ public class Boton {
     
     private String direccion; //Nos indica si la persona va asi arriba o bajo
     private boolean botonPresionado; //Saber si el botón fue presionado
+    private boolean iluminado;
 
     public Boton (String direccion){
         this.direccion = direccion;
         this.botonPresionado = false; //Inicializo false porque no se a seleccionado ninguna dirección lo que indica que no esta en funcionamiento el ascensor.
+        this.iluminado = false; //Inicializa el false porqu el botón no ha sido presionado
     }
 
     //Métodos getters y setters
@@ -27,5 +29,21 @@ public class Boton {
         this.botonPresionado = botonPresionado;
     }
 
+    //Método para saber si presionaron ya el botón
+    public void presionar(){
+        botonPresionado = true;
+        iluminar();
+        sonido();
+        System.out.println("Botón de " + direccion + " fue presionado");
+    }
+
+    public void iluminar(){
+        iluminado = true;
+        System.out.println("El botón de " + direccion + " esta iluminado");
+    }
+
+    public void sonido(){
+        System.out.println("Emitiendo sonido para " + direccion);
+    }
     
 }
