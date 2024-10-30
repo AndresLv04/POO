fac395e74090906904af3d28923fa7f7a42bcfea
Andres Lopez <166462520+AndresLv04@users.noreply.github.com>
2024-10-30 19:26:46 +0000

    Implemetación de los métodos para la puerta

--- Momento3/Puerta.java
+++ Momento3/Puerta.java
@@ -1,5 +1,33 @@
 package Momento3;
 
 public class Puerta {
+    private boolean puertaAbierta;
+
+    public Puerta(){
+        this.puertaAbierta = false;
+    }
+
+    //Método getters y setters
+    public boolean getPuertaAbierta() {
+        return puertaAbierta;
+    }
+
+    public void setPuertaAbierta(boolean puertaAbierta) {
+        this.puertaAbierta = puertaAbierta;
+    }
+
+    //Método para abrir la puerta
+    public void puertaAbierta(){
+        puertaAbierta = true;
+        System.out.println("Puerta abierta...");
+    }
+
+    //Método para cerrar la puerta
+    public void puertaCerrar(){
+        puertaAbierta = false;
+        System.out.println("Puerta cerrada...");
+    }
+
+    
     
 }
