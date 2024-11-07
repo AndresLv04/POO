75d739cc06f82c2e6df107030295fff81d506640
Andres Lopez <166462520+AndresLv04@users.noreply.github.com>
2024-11-07 04:27:22 +0000

    Agregue comentario

--- MomentoFinal/Botones/BtnsAscensor.java
+++ MomentoFinal/Botones/BtnsAscensor.java
@@ -2,6 +2,7 @@ package Botones;
 
 public class BtnsAscensor extends PlantillaBtns{
     //Método que permite saber qué botón de piso fue presionado
+    //El parámetro es para saber en a que piso quiere ir
     @Override
     public void presionar(int piso) {
         System.out.println("Presionado el piso " + piso);
